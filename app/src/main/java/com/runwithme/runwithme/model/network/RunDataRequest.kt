2e02a9e12a191fee9a63d56671bc43665f2867f5
erezlev <erezlev1994@gmail.com>
2021-08-05 17:57:51 +0300

    Last changes Erez

--- app/src/main/java/com/runwithme/runwithme/model/network/RunDataRequest.kt
+++ app/src/main/java/com/runwithme/runwithme/model/network/RunDataRequest.kt
@@ -8,20 +8,20 @@ import java.time.LocalDateTime
 import java.time.LocalTime
 
 data class RunDataRequest(
-    @SerializedName("start-time")
-    var startTime: LocalTime,
-    @SerializedName("end-time")
-    var endTime: LocalTime,
-    @SerializedName("run-date")
-    var date: LocalDateTime,
-    @SerializedName("average-pace")
-    var avgPace: SimpleDateFormat,
+    @SerializedName("startTime")
+    var startTime: String,
+    @SerializedName("endTime")
+    var endTime: String,
+    @SerializedName("runDate")
+    var date: String,
+    @SerializedName("averagePace")
+    var avgPace: String,
     @SerializedName("distance")
     var distance: Float,
     @SerializedName("steps")
     var steps: Int,
     @SerializedName("locations")
     var locations: ArrayList<Pair<Double, Double>>,
-    @SerializedName("run-type")
+    @SerializedName("runType")
     var runType: RunType = RunType.PERSONAL
 )
