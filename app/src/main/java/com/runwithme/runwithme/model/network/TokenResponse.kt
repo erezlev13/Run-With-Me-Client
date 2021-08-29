50acd8664cfd58669dc449db010ba1e264104042
shahar calfon <shaharcalfon@gmail.com>
2021-08-29 20:54:10 +0300

    bug fixes

--- app/src/main/java/com/runwithme/runwithme/model/network/TokenResponse.kt
+++ app/src/main/java/com/runwithme/runwithme/model/network/TokenResponse.kt
@@ -1,8 +1,13 @@
 package com.runwithme.runwithme.model.network
 
 import com.google.gson.annotations.SerializedName
+import com.runwithme.runwithme.model.User
 
 data class TokenResponse(
     @SerializedName("isValidToken")
-    var isValidToken: Boolean
+    var isValidToken: Boolean,
+    @SerializedName("token")
+    var token: String,
+    @SerializedName("user")
+    var user: User
 )
